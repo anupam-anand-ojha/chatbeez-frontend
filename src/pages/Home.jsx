f3f687246f267251430e7a33ad89f299d35e0b31
Anupam Anand <theanupamojha999@gmail.com>
2026-06-17 22:58:21 +0530

    update home UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
 
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-red-100">
 
       <div className="max-w-4xl mx-auto p-5">
 
