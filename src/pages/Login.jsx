a7be838b1bd914ec63b7f024c0bf13ad2a676f3f
Anupam Anand <theanupamojha999@gmail.com>
2026-07-04 17:50:50 +0530

    login form fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,19 +31,11 @@ const Login = () => {
   };
 
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse gap-10">
-        {/* Left Content */}
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Welcome Back!</h1>
-          <p className="py-6 max-w-md">
-            Login to continue accessing your account and explore all the
-            features available for you.
-          </p>
-        </div>
+    <div className="hero bg-base-200 min-h-screen ">
+      <div className="hero-content">
 
         {/* Login Card */}
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="card bg-base-100 w-[450px] min-h-[400px] shadow-2xl rounded-2xll">
           <form onSubmit={handleSubmit} className="card-body">
             <fieldset className="fieldset">
               <label className="label">Email</label>
