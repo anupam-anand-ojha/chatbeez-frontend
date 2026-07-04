f78b343c8db8bdfc4ccf4703c65f459962f27bdd
Anupam Anand <theanupamojha999@gmail.com>
2026-07-04 17:53:09 +0530

    update register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -22,67 +22,78 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const response = await api.post( "/auth/register",formData );
+      const response = await api.post("/auth/register", formData);
 
       console.log(response.data);
-
       navigate("/");
-    } 
-    catch (error) {
-      console.error(error.response?.data || error.message );
+    } catch (error) {
+      console.error(error.response?.data || error.message);
     }
   };
 
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <form
-        onSubmit={handleSubmit}
-        className="w-[400px] border p-6 rounded-lg"
-      >
-        <h1 className="text-3xl font-bold mb-6">
-          Register user 
-        </h1>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content">
+        <div className="card bg-base-100 w-[450px] shadow-2xl rounded-2xl">
+          <form onSubmit={handleSubmit} className="card-body p-8">
+            <h2 className="text-3xl font-bold text-center mb-4">
+              Create Account
+            </h2>
 
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={formData.username}
-          onChange={handleChange}
-          className="border p-3 w-full mb-4"
-        />
+            <fieldset className="fieldset">
+              <label className="label">Username</label>
+              <input
+                type="text"
+                name="username"
+                placeholder="Enter your username"
+                className="input input-bordered w-full"
+                value={formData.username}
+                onChange={handleChange}
+                required
+              />
 
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-          className="border p-3 w-full mb-4"
-        />
+              <label className="label mt-4">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+                className="input input-bordered w-full"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
 
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-          className="border p-3 w-full mb-4"
-        />
+              <label className="label mt-4">Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                className="input input-bordered w-full"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
 
-        <button
-          type="submit"
-          className="border p-3 w-full"
-        >
-          Register
-        </button>
+              <button
+                type="submit"
+                className="btn btn-neutral w-full mt-6"
+              >
+                Register
+              </button>
 
-        <p className="mt-4">
-          Already have an account?
-        {" "}
-          <Link to="/">Login</Link>
-        </p>
-      </form>
+              <p className="text-center mt-5 text-sm">
+                Already have an account?{" "}
+                <Link
+                  to="/"
+                  className="link link-primary font-medium"
+                >
+                  Login
+                </Link>
+              </p>
+            </fieldset>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
