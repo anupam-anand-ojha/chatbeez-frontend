d1fea137a8e58a58798792cbee11574279706928
Anupam Anand <theanupamojha999@gmail.com>
2026-07-28 06:30:09 +0530

    fix: update socket connection URL

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io( import.meta.env.MODE=== "development"
+     ?"http://localhost:3000"
+     :"https://chatbeez-backend.onrender.com",{
+        withCredentials: true,
+     }
+);
 
 export default socket;
